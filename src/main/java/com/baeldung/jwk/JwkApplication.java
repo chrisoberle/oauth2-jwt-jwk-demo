588f27f31a5ab5ab7901b764c8e7fa56dbfe12fd
Chris Oberle <chris.oberle@gmail.com>
2018-05-06 12:59:54 -0400

    initial import of source

---
+++ src/main/java/com/baeldung/jwk/JwkApplication.java
@@ -0,0 +1,12 @@
+package com.baeldung.jwk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwkApplication.class, args);
+	}
+}
