01558b69c27fa1e4ecc4ab155dc680ca79e73037
Chris Oberle <chris.oberle@sas.com>
2018-05-07 09:45:45 -0400

    refactor and cleanup

--- src/main/java/com/baeldung/jwk/JwkApplication.java
+++ src/main/java/com/baeldung/jwk/JwkApplication.java
@@ -2,11 +2,18 @@ package com.baeldung.jwk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JwkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JwkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JwkApplication.class, args);
+    }
+
+    @Bean
+    public static RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
